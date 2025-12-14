53f3611d4298f7bc0b75e57ff1235576460497be
MohammadAil79 <alimahdides@gmail.com>
2025-12-14 23:01:34 +0200

    Fuk 2

--- chunker.py
+++ chunker.py
@@ -1,54 +1,23 @@
-import bs4
-from langchain import hub
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders import WebBaseLoader
-from langchain_community.vectorstores import Chroma
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough
-from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+import os
+os.environ["PATH"] += os.pathsep + r"C:\ProgramData\chocolatey\lib\poppler-25.12.0\Library\bin"
+os.environ["PATH"] += os.pathsep + r"C:\Program Files\Tesseract-OCR"
+
+from unstructured.partition.pdf import partition_pdf
+from unstructured.staging.base import elements_to_json
 
 #### INDEXING ####
 
-# Load Documents
-loader = WebBaseLoader(
-    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
-    bs_kwargs=dict(
-        parse_only=bs4.SoupStrainer(
-            class_=("post-content", "post-title", "post-header")
-        )
-    ),
-)
-docs = loader.load()
+docs = "Dakhil - 2018 - Class-(9-10) English For Today PDF Web.pdf"
 
-# Split
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-splits = text_splitter.split_documents(docs)
-
-# Embed
-vectorstore = Chroma.from_documents(documents=splits, 
-                                    embedding=OpenAIEmbeddings())
-
-retriever = vectorstore.as_retriever()
-
-#### RETRIEVAL and GENERATION ####
-
-# Prompt
-prompt = hub.pull("rlm/rag-prompt")
-
-# LLM
-llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
-
-# Post-processing
-def format_docs(docs):
-    return "\n\n".join(doc.page_content for doc in docs)
-
-# Chain
-rag_chain = (
-    {"context": retriever | format_docs, "question": RunnablePassthrough()}
-    | prompt
-    | llm
-    | StrOutputParser()
+pdftransformed = partition_pdf(
+    filename=docs,
+    extract_images=True,
+    infer_table_structure=True,
+    chunking_strategy="by_characters",
+    max_characters=4000,
+    new_after_n_chars=3800,
+    combine_text_under_n_chars=2000,
+    image_output_dir_path="./Static",
 )
 
-# Question
-rag_chain.invoke("What is Task Decomposition?")
+print(pdftransformed)
